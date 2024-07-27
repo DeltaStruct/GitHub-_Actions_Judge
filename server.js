f6344b8a96a7eac2c7b0eee7117d4d91121831a3
DeltaStruct <DeltaStruct47@gmail.com>
2024-07-27 16:35:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var server = require("ws").server;
 var wss = new server({port:5000});
+const exec = require("child_process").exec;
 
 wss.on("connection", (ws) => {
   wss.on("message", (msg) => {
+    var submission = JSON.parse(msg);
+    var lang = submission.lang;
+    var code = submission.code;
+    var ret = {
+      "judge_status": "WJ",
+      "lang": "",
+      "code": "",
+      "compile_output": "",
+      "exec_time": "0"
+    };
+    ret.lang = lang;
+    ret.code = code;
+    if (lang=="C++17"){ 
+      exec("g++")
+    }
     ws.send("Judge Status: "+"TEST STATUS");
   });
 });
